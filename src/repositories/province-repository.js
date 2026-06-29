c3322c83d008f68483ea2c2732fa2721b5b5b1dc
Nicolas8701 <168933211+Nicolas8701@users.noreply.github.com>
2026-06-29 09:06:15 -0300

    guardado de emergencia

--- src/repositories/province-repository.js
+++ src/repositories/province-repository.js
@@ -20,7 +20,7 @@ export default class ProvinceRepository {
         let returnArray = null
 
         try {
-            const sql = 'SELECT * FROM provinces ORDER BY display_order'
+            const sql = 'select * from provinces order by display_order'
 
             const devolucion = await this.getDBPool().query(sql)
             returnArray = devolucion.rows
@@ -34,7 +34,7 @@ export default class ProvinceRepository {
         let returnEntity = null
 
         try {
-            const sql = 'SELECT * FROM provinces where id = $1'
+            const sql = 'select * from provinces where id = $1'
             const value = [id]
 
             const devolucion = await this.getDBPool().query(sql, value)
@@ -72,5 +72,48 @@ export default class ProvinceRepository {
         }
 
         return newId
+    }
+    updateAsync = async (id) => {
+        let rowsAffected = 0
+
+        try {
+            const sql = `update provinces
+                        set name, full_name, latitude, longitude, display_order)
+                     values
+                        ($2, $3, $4, $5, $6)
+                     where id = $1`;
+            const values = [
+                entity.name,
+                entity.full_name,
+                entity.latitude,
+                entity.longitude,
+                entity.display_order
+            ];
+
+            const devolucion = await this.getDBPool().query(sql, values)
+            rowsAffected = devolucion.rowCount;
+        } catch (error) {
+            console.log(error)
+        }
+
+        return rowsAffected
+        //1 encontró y modifico, 0 no existía
+    }
+    deleteByIdAsync = async (id) => {
+        let returnEntity = null
+
+        try {
+            const sql = 'delete * from provinces where id = $1'
+            const value = [id]
+
+            const devolucion = await this.getDBPool().query(sql, value)
+            if (devolucion.rows.length > 0) {
+                returnEntity = devolucion.rows[0]
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+        return returnEntity
     }
 }
