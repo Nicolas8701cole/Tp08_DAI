77a6dd4ae21abd62e1849478a88fd40d48841ba2
Nicolas8701 <168933211+Nicolas8701@users.noreply.github.com>
2026-06-29 10:40:20 -0300

    v3

--- database/provinces.sql
+++ database/provinces.sql
@@ -1,14 +1,15 @@
-Drop table if exists province
-create table
-    province (
+Drop table if exists province; 
+
+create table province (
         id serial primary key,
         name varchar(100) not null,
         full_name varchar(150) not null,
         latitude numeric,
         longitude numeric,
         display_order int
-    )
-insert into provinces (name, full_name, latitude, longitude, display_order) values
+    );
+	
+insert into province (name, full_name, latitude, longitude, display_order) values
 ('Ciudad Autónoma de Buenos Aires', 'Ciudad Autónoma de Buenos Aires', -34.61444091796875, -58.445877075195312, 1),
 ('Buenos Aires', 'Provincia de Buenos Aires', -36.6769415180527, -60.5588319815719, 2),
 ('Catamarca', 'Provincia de Catamarca', -27.3358332810217, -66.9476824299928, 3),
